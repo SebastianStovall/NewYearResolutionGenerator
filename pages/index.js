fbe3ad399d2068cfacc3844e8696c486b1148322
SebastianStovall <sebastianstovall@gmail.com>
2023-12-12 16:26:49 -0600

    added snow effect. worked on firework animated button, switch to confetti

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,36 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
+import { useState } from "react";
+import styles from './index.module.css'
+import FireworkButton from "@/components/ui/fireworkButton";
 
-export default function Home() {
+function Home() {
+  const [resolution, setResolution] = useState(null)
+  const [error, setError] = useState({})
 
   async function handleFetch() {
-    const res = await fetch('/api/resolutions/')
-    console.log(res)
+    const response = await fetch('/api/resolutions/')
+    if(response.ok) {
+      const resolutionData = await response.json()
+      setResolution(resolutionData)
+      setError({})
+    } else {
+      setResolution(null)
+      setError({message: 'Error Generating Resolution! Please Try Again'})
+    }
   }
 
   return (
-    <>
-      <h1>Need help finding a New Year's Resolution?</h1>
-      <button
-      onClick={handleFetch}
-      >Generate Me A Resolution</button>
-    </>
+    <div className={styles.snow_wrap}>
+      <div className={styles.snow}></div>
+
+      <h1 className={styles.centerHeading}>Need help finding a New Year's Resolution?</h1>
+      <div className={styles.center}>
+        <button onClick={handleFetch} className={styles.resolutionButton}>Generate Me A Resolution</button>
+      </div>
+      {resolution && <p className={styles.centerText}>{resolution.prompt}</p>}
+      {Object.values(error).length > 0 && <p>{error.message}</p>}
+    </div>
   )
 }
+
+
+export default Home;
