d76f26299203aac339700bc3b2752cde85dc9bdf
JamesAskelson <jamesaskelson630@gmail.com>
2023-12-12 18:23:54 -0600

    finished-new-years-countdown

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import styles from './index.module.css'
 import Confetti from 'react-confetti';
+import CountDown from "@/components/countdown";
+import getYear from "@/lib/getNewYear";
 
 function Home() {
   const [resolution, setResolution] = useState(null)
   const [isConfettiActive, setIsConfettiActive] = useState(false);
   const [error, setError] = useState({})
+  const year = getYear()
+
 
   async function handleFetch() {
     const response = await fetch('/api/resolutions/')
@@ -29,8 +33,10 @@ function Home() {
     <div className={styles.snow_wrap}>  {/* image background */}
       {isConfettiActive && <Confetti />} {/* confetti effect */}
       <div className={styles.snow}></div> {/* snow effect */}
-      
+
       <h1 className={styles.centerHeading}>Need help finding a New Year's Resolution?</h1>
+      <h2 className={styles.centerCountdownTxt}>Clocks Ticking!</h2>
+      <CountDown newYear={year}></CountDown>
       <div className={styles.center}>
         <button onClick={handleFetch} className={styles.resolutionButton}>Generate Me A Resolution</button>
       </div>
